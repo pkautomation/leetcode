9c054cb65e5c29e3b622f2ebf20ffd37f2c66b70
Pawel Kreski <pawel.kreski@yahoo.com>
2024-08-25 12:58:16 +0200

    initial work

---
+++ leetcode_solutions_pack/TwoSum/Program.cs
@@ -0,0 +1,23 @@
+﻿// https://leetcode.com/problems/two-sum/
+
+int[] TwoSum(int[] nums, int target)
+    {
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+
+                if (nums[i] + nums[j] == target)
+                {
+                return [i, j];
+                }
+            }
+        }
+
+        return nums;
+    }
+
+var res = TwoSum([1, 2, 3], 4);
+
+if (!res.SequenceEqual([0, 2])) throw new Exception("Epic fail");
