567d6f780bbac4afa8a3120f11b1145053225a50
Pawel Kreski <pawel.kreski@yahoo.com>
2024-08-31 16:40:52 +0200

    optimize code

--- leetcode_solutions_pack/ValidParantheses/Program.cs
+++ leetcode_solutions_pack/ValidParantheses/Program.cs
@@ -1,51 +1,31 @@
 ﻿// https://leetcode.com/problems/valid-parentheses/
 
-bool IsValid(string s) {
+bool IsValid(string s)
+{
     Stack<char> stack = new();
 
     foreach (char currentBracket in s)
     {
-        if (currentBracket.IsOpeningBracket())
+        switch (currentBracket)
         {
-            stack.Push(currentBracket);
-        }
-        else
-        {
-            if (stack.Count == 0)
-            {
-                return false;
-            }
-
-            var elem = stack.Peek();
-
-            if (elem.IsMatchClosingBracket(currentBracket))
-            {
-                stack.Pop();
-            }
-            else
-            {
-                return false;
-            }
+            case '{':
+            case '[':
+            case '(':
+                stack.Push(currentBracket);
+                break;
+            case '}':
+                if (stack.Count == 0 || stack.Pop() != '{') return false;
+                break;
+            case ']':
+                if (stack.Count == 0 || stack.Pop() != '[') return false;
+                break;
+            case ')':
+                if (stack.Count == 0 || stack.Pop() != '(') return false;
+                break;
+            default:
+                return false; // Invalid character
         }
     }
 
     return stack.Count == 0;
-}
-    
-
-
-static class Extensions
-{
-    public static bool IsMatchClosingBracket(this char openingBracket, char closingBracket)
-    {
-        if (openingBracket == '{' && closingBracket == '}') return true;
-        if (openingBracket == '[' && closingBracket == ']') return true;
-        if (openingBracket == '(' && closingBracket == ')') return true;
-        return false;
-    }
-
-    public static bool IsOpeningBracket(this char bracket)
-    {
-        return bracket == '{' || bracket == '[' || bracket == '(';
-    }
 }
