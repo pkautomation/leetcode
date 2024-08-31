d0033425e4d299283c4ec9e8a6ba8be305ec4248
Pawel Kreski <pawel.kreski@yahoo.com>
2024-08-31 16:33:16 +0200

    valid parentheses problem

---
+++ leetcode_solutions_pack/ValidParantheses/Program.cs
@@ -0,0 +1,51 @@
+﻿// https://leetcode.com/problems/valid-parentheses/
+
+bool IsValid(string s) {
+    Stack<char> stack = new();
+
+    foreach (char currentBracket in s)
+    {
+        if (currentBracket.IsOpeningBracket())
+        {
+            stack.Push(currentBracket);
+        }
+        else
+        {
+            if (stack.Count == 0)
+            {
+                return false;
+            }
+
+            var elem = stack.Peek();
+
+            if (elem.IsMatchClosingBracket(currentBracket))
+            {
+                stack.Pop();
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    return stack.Count == 0;
+}
+    
+
+
+static class Extensions
+{
+    public static bool IsMatchClosingBracket(this char openingBracket, char closingBracket)
+    {
+        if (openingBracket == '{' && closingBracket == '}') return true;
+        if (openingBracket == '[' && closingBracket == ']') return true;
+        if (openingBracket == '(' && closingBracket == ')') return true;
+        return false;
+    }
+
+    public static bool IsOpeningBracket(this char bracket)
+    {
+        return bracket == '{' || bracket == '[' || bracket == '(';
+    }
+}
